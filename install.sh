07f7fbbba6a50301f098970ac361fecfdcf8b334
Nikolaj Schaldemose Reibke <nikolajsr12@gmail.com>
2024-06-15 00:05:47 +0200

    avoided nested install

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 sudo mkdir -p /etc/local/bin/wg_exporter
-sudo cp -r wg_exporter /etc/local/bin/wg_exporter/
+cd wg_exporter
+sudo cp -r * /etc/local/bin/wg_exporter/
+cd ..
 sudo chown -R wgexporter:wgexporter /etc/local/bin/wg_exporter
